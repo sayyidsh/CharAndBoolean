f38694db69febb49700ca140e3f39e61dbe43800
Sayyid Shaban <sayyid.ls@gail.com>
2018-01-07 16:26:04 +0200

    Initial commit

---
+++ src/pro/nanosystems/Main.java
@@ -0,0 +1,12 @@
+package pro.nanosystems;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char myChar = '\u00A9';
+        System.out.println(myChar);
+        boolean myBoolean = true;
+        char myRegisterSymbol = '\u00AE';
+        System.out.println(myRegisterSymbol);
+    }
+}
